947ae09d1f2806e71417098dc78df1953253e13c
geraldanthony6 <gsquare600@gmail.com>
2024-03-02 15:20:09 -0600

    super simple police movement done

--- Assets/Scripts/PoliceMovement.cs
+++ Assets/Scripts/PoliceMovement.cs
@@ -7,12 +7,6 @@ public class PoliceMovement : MonoBehaviour
 {
     private GameObject Player;
 
-    [SerializeField] private Transform[] WanderPoints;
-
-    [SerializeField] private Transform CurrentWanderPoint;
-
-    [SerializeField] private int CurrentWanderIndex;
-
     [SerializeField] private float DistanceToPlayer;
 
     [SerializeField] private float ViewRadius;
@@ -29,28 +23,13 @@ public class PoliceMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CurrentWanderIndex = 0;
-        CurrentWanderPoint = WanderPoints[CurrentWanderIndex];
+        Player = FindObjectOfType<PlayerMovement>().gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        DistanceToPlayer = Vector2.Distance(transform.position, CurrentWanderPoint.position);
-        if (DistanceToPlayer > 1.0f)
-        {
-            transform.position = Vector2.MoveTowards(transform.position ,CurrentWanderPoint.position, 2.0f * Time.deltaTime);
-        }
-
-        if (DistanceToPlayer <= 1.0f && CurrentWanderIndex != WanderPoints.Length - 1)
-        {
-            CurrentWanderIndex++;
-            CurrentWanderPoint = WanderPoints[CurrentWanderIndex];
-        } else if (DistanceToPlayer <= 1.0f && CurrentWanderIndex == WanderPoints.Length - 1)
-        {
-            CurrentWanderIndex = 0;
-            CurrentWanderPoint = WanderPoints[CurrentWanderIndex];
-        }
+        transform.position = Vector2.MoveTowards(transform.position, Player.transform.position, 6.0f * Time.deltaTime);
         
         FindVisibleTargets();
     }
