b8ae49876a2b37ab11fd50793178a6fdb2b9dca5
Sphere39 <77363797+Sphere39@users.noreply.github.com>
2024-03-02 14:05:34 -0600

    Inputed Main Menu Art and Mechanics

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [field: SerializeField]
+    private Image spriteRenderer;
+    [field: SerializeField]
+    private Sprite replacementSprite;
+    public void PlayGame()
+    {
+        spriteRenderer.sprite = replacementSprite;
+        SceneManager.LoadSceneAsync("SampleScene");
+    }
+
+    public void Tutorial()
+    {
+        //spriteRenderer.sprite = replacementSprite;
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        //spriteRenderer.sprite = replacementSprite;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
+
